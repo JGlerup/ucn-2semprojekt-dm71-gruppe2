4658baf3903234b8b9bd539a5143e3e73455452e
erik.gravesen <erik.gravesen@951ca573-ac9d-39a4-165f-cb97d4f8997a>
2010-10-19 07:34:03 +0000

    yoyo, 1st upload, i r so cool

---
+++ trunk/BostedProofOfConcept/src/ModelLayer/Employee.java
@@ -0,0 +1,77 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ModelLayer;
+
+/**
+ *
+ * @author Erik
+ */
+public class Employee {
+
+    private String fname;
+    private String lname;
+    private String initial;
+    private String adresse;
+    private int tlfnr;
+    private String email;
+
+    public Employee(String fname, String lname, String initial, String adresse, int tlfnr, String email) {
+        this.fname = fname;
+        this.lname = lname;
+        this.initial = initial;
+        this.adresse = adresse;
+        this.tlfnr = tlfnr;
+        this.email = email;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public String getInitial() {
+        return initial;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public int getTlfnr() {
+        return tlfnr;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public void setInitial(String initial) {
+        this.initial = initial;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public void setTlfnr(int tlfnr) {
+        this.tlfnr = tlfnr;
+    }
+}

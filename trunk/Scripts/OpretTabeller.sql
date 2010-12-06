798deadf9e1a74811065943b56d2aebfe1aba66c
glerup9@hotmail.com <glerup9@hotmail.com@951ca573-ac9d-39a4-165f-cb97d4f8997a>
2010-12-06 11:16:43 +0000

    Scripts/OpretTabeller.sql og Scripts/indsætTabellerQuary.sql ændret og tilføjet

--- trunk/Scripts/OpretTabeller.sql
+++ trunk/Scripts/OpretTabeller.sql
@@ -1,16 +1,16 @@
+use dm71_2
 create table location 
-	(location_id		int			not null,
+	(location_id		int			PRIMARY KEY IDENTITY,
 	zipcode				int			not null,
 	city				varchar(15)	not null,
-	primary key (location_id),
 	unique(zipcode));
 
 create table frequency 
-	(frequency_id		int			not null,
+	(frequency_id		int			PRIMARY KEY IDENTITY,
 	time				varchar(30)	not null,
 	quantity			int			not null,
 	date				datetime	not null,
-	primary key (frequency_id));
+	);
 
 create table car 
 	(car_id				int			not null,
@@ -20,8 +20,8 @@ create table car
 	unique(registrationsno));
 
 create table externalcontact 
-	(externalcontact_id	int			not null,
-	cbr					int,
+	(externalcontact_id	int			PRIMARY KEY IDENTITY,
+	cbr					varchar(20),
 	occupation			varchar(30)	not null,
 	firstname			varchar(15)	not null,
 	middlename			varchar(15),
@@ -30,16 +30,15 @@ create table externalcontact
 	location_id			int			not null,
 	phoneno				int,
 	email				varchar(50),
-	primary key (externalcontact_id),
 	foreign key (location_id) references location(location_id));
 
 create table client 
-	(client_id			int			not null,
+	(client_id			int			PRIMARY KEY IDENTITY,
 	clientno			int			not null,
 	description			varchar(50) not null,
 	interests			varchar(50) not null,
 	health				varchar(50) not null,
-	ssn					int			not null,
+	ssn					varchar(11)			not null,
 	firstname			varchar(15)	not null,
 	middlename			varchar(15),
 	lastname			varchar(15)	not null,
@@ -47,16 +46,16 @@ create table client
 	location_id			int			not null,
 	phoneno				int,
 	email				varchar(50),
-	primary key (client_id),
 	unique(ssn),
 	foreign key (location_id) references location(location_id));
 
 create table employee 
-	(employee_id		int			not null,
-	employeeno			int			not null,
-	managerno			int,
+	(employee_id		int			PRIMARY KEY IDENTITY,
+	employeeno			varchar(15)	not null,
+	password			varchar(15) not null,
+	managerno			varchar(15),
 	jobtitle			varchar(30) not null,
-	ssn					int			not null,
+	ssn					varchar(11)			not null,
 	firstname			varchar(15)	not null,
 	middlename			varchar(15),
 	lastname			varchar(15)	not null,
@@ -64,14 +63,13 @@ create table employee
 	location_id			int			not null,
 	phoneno				int,
 	email				varchar(50),
-	primary key (employee_id),
 	unique(employeeno),
 	unique(ssn),
 	foreign key (managerno) references employee(employeeno),
 	foreign key (location_id) references location(location_id));
 	
 create table medicine 
-	(medicine_id		int			not null,
+	(medicine_id		int			PRIMARY KEY IDENTITY,
 	frequency_id		int			not null,
 	externalcontact_id	int			not null,
 	client_id			int			not null,
@@ -79,78 +77,70 @@ create table medicine
 	description			varchar(50)	not null,
 	date				datetime	not null,
 	quantity			int			not null,
-	primary key (medicine_id),
 	foreign key (frequency_id) references frequency(frequency_id),
 	foreign key (externalcontact_id) references externalcontact(externalcontact_id),
 	foreign key (client_id) references client(client_id));
 
 create table dailyreport
-	(dailyreport_id		int			not null,
+	(dailyreport_id		int			PRIMARY KEY IDENTITY,
 	client_id			int			not null,
 	employee_id			int			not null,
 	text				varchar(8000)not null,
 	date				datetime	not null,
-	primary key (dailyreport_id),
 	foreign key (client_id) references client(client_id),
 	foreign key (employee_id) references employee(employee_id));
 
 create table reservation
-	(reservation_id		int			not null,
+	(reservation_id		int			PRIMARY KEY IDENTITY,
 	car_id				int			not null,
 	employee_id			int			not null,
 	client_id			int			not null,
 	startdate			datetime	not null,
 	enddate				datetime	not null,
 	reservationdate		datetime	not null,
-	primary key (reservation_id),
 	foreign key (car_id) references car(car_id),
 	foreign key (employee_id) references employee(employee_id),
 	foreign key (client_id) references client(client_id));
 
 create table externaldocument
-	(externaldocument_id int		not null,
+	(externaldocument_id int		PRIMARY KEY IDENTITY,
 	client_id			int			not null,
 	text				varchar(8000)not null,
 	date				datetime	not null,
 	source				varchar(50),
 	author				varchar(50),
-	primary key (externaldocument_id),
 	foreign key (client_id) references client(client_id));
 
 create table milestone
-	(milestone_id		int			not null,
+	(milestone_id		int			PRIMARY KEY IDENTITY,
 	client_id			int			not null,
 	text				varchar(8000)not null,
 	date				datetime	not null,
 	successdate			datetime,
-	primary key (milestone_id),
 	foreign key (client_id) references client(client_id));
 
 create table errorhandlingmedicine
-	(errorhandlingmedicine_id int	not null,
+	(errorhandlingmedicine_id int	PRIMARY KEY IDENTITY,
 	medicine_id			int			not null,
 	client_id			int			not null,
 	employee_id			int			not null,
 	date				datetime	not null,
 	episode				varchar(8000)not null,
 	quantity			int			not null,
-	primary key (errorhandlingmedicine_id),
 	foreign key (medicine_id) references medicine(medicine_id),
 	foreign key (client_id) references client(client_id),
 	foreign key (employee_id) references employee(employee_id));
 
 create table client_externalcontact
-	(client_externalcontact_id int	not null,
+	(client_externalcontact_id int	PRIMARY KEY IDENTITY,
 	client_id			int			not null,
 	externalcontact_id	int			not null,
-	primary key (client_externalcontact_id),
 	foreign key (client_id) references client(client_id),
 	foreign key (externalcontact_id) references externalcontact(externalcontact_id));
 
 create table employee_client
-	(employee_client_id int			not null,
+	(employee_client_id int			PRIMARY KEY IDENTITY,
 	employee_id			int			not null,
 	client_id			int			not null,
-	primary key (employee_client_id),
 	foreign key (employee_id) references employee(employee_id),
 	foreign key (client_id) references client(client_id));

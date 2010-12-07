fd457a0c9b7e44a6ad77af5eefd7ddf3e0e2b566
anitalykkeclemmensen <anitalykkeclemmensen@951ca573-ac9d-39a4-165f-cb97d4f8997a>
2010-12-07 13:57:45 +0000

    Location.java

---
+++ trunk/Bosted/src/ModelLayer/Location.java
@@ -0,0 +1,56 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ModelLayer;
+
+/**
+ *
+ * @author Anita
+ */
+public class Location
+{
+    private int locationID;
+    private int zipcode;
+    private String city;
+
+    public Location()
+    {
+
+    }
+
+
+    public Location (int locationID, int zipcode, String city)
+    {
+
+    this.locationID = locationID;
+    this.zipcode = zipcode;
+    this.city = city;
+    }
+
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getLocationID() {
+        return locationID;
+    }
+
+    public void setLocationID(int locationID) {
+        this.locationID = locationID;
+    }
+
+    public int getZipCode() {
+        return zipcode;
+    }
+
+    public void setZipCode(int zipcode) {
+        this.zipcode = zipcode;
+    }
+}
